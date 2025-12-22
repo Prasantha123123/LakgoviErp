2747f916de7b0c2a817dbee2cf63fa71d0859ab5
Prasantha <prasantha20011216@gmail.com>
2025-12-22 11:03:24 +0530

    db fix

--- database.php
+++ database.php
@@ -3,8 +3,8 @@
 class Database {
     private $host = 'localhost';
     private $db_name = 'lakgovi_erp';
-    private $username = 'root';
-    private $password = '';
+     private $username = 'dbuser';
+    private $password = 'L{582Phb1Lh5';
     private $conn;
 
     public function getConnection() {
@@ -14,7 +14,7 @@ class Database {
                                 $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $exception) {
-            throw new Exception("Connection error: " . $exception->getMessage());
+            echo "Connection error: " . $exception->getMessage();
         }
         return $this->conn;
     }
